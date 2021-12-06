b863fe4cfce8eee6518b7d41b0ae41f48d6806c5
Adam Szentirmai-Schon <adamsan3@gmail.com>
2021-12-06 06:59:20 +0100

    initial commit, add pom with junit, gitignore, empty App and test classes

---
+++ src/test/java/com/codecool/mi6/AppTest.java
@@ -0,0 +1,12 @@
+package com.codecool.mi6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testMain() {
+    }
+}
